5262958fd0b530930d3f4062f3c043fd6484d300
snowleung <snowleung@gmail.com>
2014-03-26 18:22:42 +0800

    Initial Commit

---
+++ jinziqi/SnAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SnAppDelegate.h
+//  jinziqi
+//
+//  Created by snow on 26/3/14.
+//  Copyright (c) 2014 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ jinziqi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  jinziqi
+//
+//  Created by snow on 26/3/14.
+//  Copyright (c) 2014 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SnAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SnAppDelegate class]));
+    }
+}
