fb016745a6f27731e8257e71c7d278680cbfb530
snowleung <snowleung@gmail.com>
2014-03-27 21:07:43 +0800

    add player

---
+++ jinziqi/SnChessboradViewController.h
@@ -0,0 +1,13 @@
+//
+//  SnChessboradViewController.h
+//  jinziqi
+//
+//  Created by snow on 27/3/14.
+//  Copyright (c) 2014 snow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SnChessboradViewController : UIViewController
+
+@end
---
+++ jinziqi/SnChessboradViewController.m
@@ -0,0 +1,38 @@
+//
+//  SnChessboradViewController.m
+//  jinziqi
+//
+//  Created by snow on 27/3/14.
+//  Copyright (c) 2014 snow. All rights reserved.
+//
+
+#import "SnChessboradViewController.h"
+
+@interface SnChessboradViewController ()
+
+@end
+
+@implementation SnChessboradViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
